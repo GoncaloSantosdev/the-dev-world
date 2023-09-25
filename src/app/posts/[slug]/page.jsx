400ceaa7d36792c622b2aa681f1a3cd2c41b099b
GoncaloSantosdev <goncalosantosprofissional@gmail.com>
2023-09-25 10:29:55 +0100

    fetch single post

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,14 +1,27 @@
 import { Sidebar, Title } from "@/components";
 import Image from "next/image";
 
-const SinglePostPage = () => {
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const SinglePostPage = async ({ params }) => {
+  const { slug } = params;
+
+  const data = await getData(slug);
   return (
     <section>
       <div className="flex flex-col md:flex-row items-center gap-12 mt-12">
         <div className="flex-1 space-y-8">
-          <h2 className="text-4xl font-semibold">
-            Easiest way for React state management
-          </h2>
+          <h2 className="text-4xl font-semibold">{data?.title}</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque,
             ab. Distinctio id nisi laudantium ipsa numquam iure ratione
@@ -16,14 +29,14 @@ const SinglePostPage = () => {
           </p>
           <div className="flex items-center gap-4">
             <Image
-              src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3006&q=80"
-              width={100}
-              height={100}
-              className="w-[60px] h-[60px] rounded-full"
+              src={data?.user.image}
+              width={80}
+              height={80}
+              className="w-[50px] h-[50px] rounded-full"
               alt="post"
             />
             <p>
-              User Name <br /> 21.08.2023
+              {data?.user.name} <br /> 21.08.2023
             </p>
           </div>
         </div>
@@ -41,54 +54,12 @@ const SinglePostPage = () => {
       </div>
 
       <div className="flex flex-col lg:flex-row mt-24 gap-6 flex-1">
-        <div className="lg:w-[70%] space-y-6">
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi id
-            eaque ducimus repellat, ut dolores dolorem animi corporis, velit
-            vero cum blanditiis quod vitae, enim explicabo tempore ipsum.
-            Officiis quis libero quod ex aspernatur vero culpa atque, et
-            deserunt, adipisci debitis rerum quas quisquam? Ab sit nemo eligendi
-            maiores dicta sapiente soluta quidem, officia optio, odio aliquam
-            quia fugiat voluptatem.
-          </p>
-          <h3 className="text-lg font-semibold">
-            Why should you learn Zustand?
-          </h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi id
-            eaque ducimus repellat, ut dolores dolorem animi corporis, velit
-            vero cum blanditiis quod vitae, enim explicabo tempore ipsum.
-            Officiis quis libero quod ex aspernatur vero culpa atque, et
-            deserunt, adipisci debitis rerum quas quisquam? Ab sit nemo eligendi
-            maiores dicta sapiente soluta quidem, officia optio, odio aliquam
-            quia fugiat voluptatem.
-          </p>
-          <div className="relative h-[50vh] w-full">
-            <Image
-              src={
-                "https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3006&q=80"
-              }
-              fill
-              className="absolute h-full object-cover rounded shadow"
-              alt="Post"
-              priority
-            />
-          </div>
-          <h3 className="text-lg font-semibold">
-            Why should you learn Zustand?
-          </h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi id
-            eaque ducimus repellat, ut dolores dolorem animi corporis, velit
-            vero cum blanditiis quod vitae, enim explicabo tempore ipsum.
-          </p>
-          <p>
-            Officiis quis libero quod ex aspernatur vero culpa atque, et
-            deserunt, adipisci debitis rerum quas quisquam? Ab sit nemo eligendi
-            maiores dicta sapiente soluta quidem, officia optio, odio aliquam
-            quia fugiat voluptatem.
-          </p>
-          <div>
+        <div className="lg:w-[70%]">
+          <div
+            className="space-y-6"
+            dangerouslySetInnerHTML={{ __html: data?.desc }}
+          />
+          <div className="mt-12">
             <Title title={"Comments"} />
 
             <div className="mt-8 space-y-8">
@@ -102,7 +73,7 @@ const SinglePostPage = () => {
                     alt="post"
                   />
                   <p>
-                    User Name <br /> 21.08.2023
+                    {data?.user.name} <br /> 21.08.2023
                   </p>
                 </div>
 
