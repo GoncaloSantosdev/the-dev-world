1356e9e2afb6e40f93c40eac6400fe204a08621c
GoncaloSantosdev <goncalosantosprofissional@gmail.com>
2023-09-25 16:34:59 +0100

    ready to deploy

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,8 +6,8 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE,
-  authDomain: "nextjs-blog-ff64e.firebaseapp.com",
-  projectId: "nextjs-blog-ff64e",
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: "nextjs-blog-ff64e.appspot.com",
   messagingSenderId: "330264727809",
   appId: "1:330264727809:web:5d1ef9b2b4424cfec20b00",
